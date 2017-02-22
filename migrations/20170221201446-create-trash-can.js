fed7d78b1af9819003102bf987bf2e402fb19640
Gazy Mahomar <gmahomarf@gmail.com>
2017-02-22 10:40:24 -0600

    Added postgis setup

--- migrations/20170221201446-create-trash-can.js
+++ migrations/20170221201446-create-trash-can.js
@@ -2,30 +2,33 @@
 
 module.exports = {
 	up: function(queryInterface, Sequelize) {
-		return queryInterface.createTable('TrashCans', {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: Sequelize.INTEGER
-			},
-			location: {
-				type: Sequelize.GEOMETRY('POINT')
-			},
-			validatedCount: {
-				type: Sequelize.INTEGER,
-				defaultValue: 1,
-				allowNull: false
-			},
-			createdAt: {
-				allowNull: false,
-				type: Sequelize.DATE
-			},
-			updatedAt: {
-				allowNull: false,
-				type: Sequelize.DATE
-			}
-		});
+		return queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS postgis;')
+			.then(() => {
+				return queryInterface.createTable('TrashCans', {
+					id: {
+						allowNull: false,
+						autoIncrement: true,
+						primaryKey: true,
+						type: Sequelize.INTEGER
+					},
+					location: {
+						type: Sequelize.GEOMETRY('POINT')
+					},
+					validatedCount: {
+						type: Sequelize.INTEGER,
+						defaultValue: 1,
+						allowNull: false
+					},
+					createdAt: {
+						allowNull: false,
+						type: Sequelize.DATE
+					},
+					updatedAt: {
+						allowNull: false,
+						type: Sequelize.DATE
+					}
+				});
+			});
 	},
 	down: function(queryInterface, Sequelize) {
 		return queryInterface.dropTable('TrashCans');
